4eecd76f8389e8cedb21df019ea399dd7abaf607
viktoriagalver <56873841+viktoriagalver@users.noreply.github.com>
2020-11-11 16:47:12 +0100

    added gas bubble

--- visualization/sketch.js
+++ visualization/sketch.js
@@ -4,6 +4,8 @@ let hum = '';
 
 let temp = '';
 
+let gas = '';
+
 ws.onopen = function (event){
   console.log(event);
 }
@@ -12,7 +14,7 @@ ws.onmessage = (msg) => {
   const data = JSON.parse(msg.data);
   hum = data.humidity*5;
   temp = data.temperature;
-  gas = data.gas;
+  gas = data.gas*3;
   //if (data.length > 100){
     //data.shift();
     console.log(data);
@@ -28,10 +30,19 @@ function setup() {
 function draw() {
   let tempColor = map(temp*25, 0, width, 0, 255);
   background(tempColor, 0, 0);
-  stroke(0, 153, 255)
-  fill(88, 187, 237)
-  strokeWeight(6)
-  ellipse(x, y, hum, hum)
+
+blendMode(HARD_LIGHT)
+
+stroke(34, 128, 27)
+strokeWeight(6)
+fill(70, 179, 62)
+ellipse(x, y, gas, gas)
+
+stroke(0, 153, 255)
+fill(88, 187, 237)
+strokeWeight(6)
+ellipse(x, y, hum, hum)
+
 
 
   
