3759bea7bff9610ec79783ad95c8596c45b40a98
viktoriagalver <56873841+viktoriagalver@users.noreply.github.com>
2020-11-12 16:13:15 +0100

    fixed bugs

--- visualization/sketch.js
+++ visualization/sketch.js
@@ -28,24 +28,30 @@ function setup() {
 
 
 function draw() {
-  let tempColor = map(temp*25, 0, width, 0, 255);
-  background(tempColor, 0, 0);
-
-blendMode(HARD_LIGHT)
-
-stroke(34, 128, 27)
-strokeWeight(6)
-fill(70, 179, 62)
-ellipse(x, y, gas, gas)
-
-stroke(0, 153, 255)
-fill(88, 187, 237)
-strokeWeight(6)
-ellipse(x, y, hum, hum)
-
-
-
   
+  const tempMin = 25;
+  const tempMax = 30;
+  let tempColor = temp < tempMin ? 0 : temp > tempMax ? color(255, 0, 0) : color(`hsb(0, 100%, ${map(temp, tempMin, tempMax, 0, 100)}%)`);
 
-  
+  background(tempColor);
+
+  if (hum < gas) {
+    stroke(34, 128, 27);
+    strokeWeight(6);
+    fill(70, 179, 62);
+    ellipse(x, y, gas, gas);
+    stroke(0, 153, 255);
+    strokeWeight(6);
+    fill(88, 187, 237);
+    ellipse(x, y, hum, hum);
+  } else {
+    stroke(0, 153, 255);
+    strokeWeight(6);
+    fill(88, 187, 237);
+    ellipse(x, y, hum, hum);
+    stroke(34, 128, 27);
+    strokeWeight(6);
+    fill(70, 179, 62);
+    ellipse(x, y, gas, gas);
+  }
 }
